64b59cfdb949b03657ebb30ed13db4f75b86ce36
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 15:06:38 +0530

    IPolicyService.java

--- src/dao/IPolicyService.java
+++ src/dao/IPolicyService.java
@@ -3,9 +3,9 @@ import entity.Policy;
 import java.util.List;
 
 public interface IPolicyService {
-    boolean createNewPolicy(Policy policy); // renamed
-    Policy findPolicyById(int policyId);    // renamed
-    List<Policy> fetchAllInsurancePlans();  // renamed
-    boolean updateExistingPolicy(Policy policy); // renamed
+    boolean createNewPolicy(Policy policy); 
+    Policy findPolicyById(int policyId);    
+    List<Policy> fetchAllInsurancePlans(); 
+    boolean updateExistingPolicy(Policy policy); 
     boolean removePolicy(int policyId);     // renamed
 }
